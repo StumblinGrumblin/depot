b77f582d24df9a94888ca208e1065378173d52d4
StumblinGrumblin <gary@sweetlabs.com>
2015-03-14 20:39:23 -0700

    Added user accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
-  resources :line_items
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :orders
+  resources :line_items
   resources :carts
 
   get 'store/index'
-
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
